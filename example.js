c1b08b9f59a70817d7c29ccdd0893f2260022deb
Brem0827 <62270266+Brem0827@users.noreply.github.com>
2022-12-01 12:46:04 +0900

    button add

--- example.js
+++ example.js
@@ -1,18 +1,26 @@
-const imagelement = document.querySelector('img');
-const onClickListner = (a, event) => {
-    const nowSrc = imagelement.getAttribute('src');
-    console.log(nowSrc);
+const imageElement = document.querySelector('img');
+const buttonElement = document.querySelector('button');
+const h1Element = document.querySelector('h1');
 
+const onClickListner = () => {
+    const nowSrc = imageElement.getAttribute('src');
+    //console.log(nowSrc);
     if(nowSrc === 'image/game_1.jpg'){
-        imagelement.setAttribute('src', 'image/game_2.jpg');
+        imageElement.setAttribute('src', 'image/game_2.jpg');
     }else{
-        imagelement.setAttribute('src', 'image/game_1.jpg');
+        imageElement.setAttribute('src', 'image/game_1.jpg');
     }
     //console.log(event);
     //console.log(a);
     alert("클릭 되었습니다!");
 };
 
+const onClickButtonListner = () => {
+    const userName = prompt("이름을 입력 해 주세요");
+    h1Element.innerText = userName;
+}
+
 //imagelement.onmousemove = onClickListner;
-imagelement.onclick = onClickListner;
+imageElement.onclick = onClickListner;
+buttonElement.onclick = onClickButtonListner;
 console.log(imgelement);
